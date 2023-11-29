bc5c093f7ca97cca7048d96727c7f7624d712957
linuxpedro <pdro.dev97@gmail.com>
2023-11-29 00:10:07 -0300

    feat: new function in controller

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,11 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function contato() {
-        var_dump($_POST);
+    public function contato(Request $request) {
+        /*
+        $request->all();
+        Posso selecionar apenas um input
+        $request->input('nome');
+        
+
+        $contato = new SiteContato();
+        $contato->fill($request->all());
+        $contato->save();
+        */
+        //print_r($contato->getAttributes());
+
+
+
         return view('site.contato');
+    }
+
+    public function save(Request $request) {
+        $request->validate([
+            'nome'           => 'required|string|unique:site_contatos,nome',
+            'telefone'       => 'required|string|max:12',
+            'email'          => 'required|email',
+            'motivo_contato' => 'required|string',
+            'mensagem'       => 'required|string|max:255',
+        ]);
     }
 }
