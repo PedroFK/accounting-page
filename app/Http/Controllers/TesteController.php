92b5f8922da805c38730c6fb8fe98121882bce73
linuxpedro <pdro.dev97@gmail.com>
2023-12-10 15:48:00 -0300

    feat: teste model

---
+++ app/Http/Controllers/TesteController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTesteRequest;
+use App\Http\Requests\UpdateTesteRequest;
+use App\Models\Teste;
+
+class TesteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTesteRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Teste $teste)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTesteRequest $request, Teste $teste)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Teste $teste)
+    {
+        //
+    }
+}
