e6258679eca193cf0e70f3ad40f54dbfc87f8b59
linuxpedro <pdro.dev97@gmail.com>
2023-12-13 19:56:09 -0300

    feat: middleware access log about us

--- app/Http/Controllers/AboutUsController.php
+++ app/Http/Controllers/AboutUsController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Middleware\AccessLogMiddleware;
 
 class AboutUsController extends Controller
 {
+    public function __construct() {
+        $this->middleware(AccessLogMiddleware::class);
+    }
+
     public function aboutUs() {
         return view('site.about-us');
     }
