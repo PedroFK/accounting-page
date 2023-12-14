74389ec88ff06787c3e84bae8f458fab5e696cb4
linuxpedro <pdro.dev97@gmail.com>
2023-12-14 20:43:14 -0300

    feat: middleware nickname

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,7 +36,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\AccessLogMiddleware::class
+            \App\Http\Middleware\AccessLogMiddleware::class,
         ],
 
         'api' => [
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'access.log' => \App\Http\Middleware\AccessLogMiddleware::class,
     ];
 }
