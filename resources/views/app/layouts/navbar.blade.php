d38b87dc0b9bfbd26bc2c227850b73e7d3dc05de
linuxpedro <pdro.dev97@gmail.com>
2023-12-23 16:01:27 -0300

    feat: private navbar

---
+++ resources/views/app/layouts/navbar.blade.php
@@ -0,0 +1,15 @@
+<div class="top">
+
+  <div class="logo">
+      <img src={{ asset('img/logo.png') }}>
+  </div>
+
+  <div class="menu">
+      <ul>
+          <li><a href="{{ route('app.home') }}" style="color: white;">Principal</a></li>
+          <li><a href="{{ route('app.client') }}" style="color: white;">Cliente</a></li>
+          <li><a href="{{ route('app.supplier') }}" style="color: white;">Fornecedor</a></li>
+          <li><a href="{{ route('app.product') }}" style="color: white;">Produto</a></li>
+      </ul>
+  </div>
+</div>
