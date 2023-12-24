fc7fbad08687159da44b21640debcf893e262713
linuxpedro <pdro.dev97@gmail.com>
2023-12-24 08:12:03 -0300

    fix: menu style

--- resources/views/app/supplier/index.blade.php
+++ resources/views/app/supplier/index.blade.php
@@ -8,25 +8,27 @@
         <p>Fornecedor</p>
     </div>
 
-    <div class="menu" style="margin-top: 100px;">
-        <ul>
-            <li>
-                <a href="#" style="color: white;">Novo</a>
-                <a href="#" style="color: white;">Consulta</a>
-            </li>
-        </ul>
-    </div>
-
-    <div class="page-information blue-ocean">
-        <div style="width: 30%; margin-left: auto; margin-right: auto; color: black;">
-            <form method="post" action="">
-                <input type="text" name="name" placeholder="Nome" class="black-border" id="">
-                <input type="text" name="site" placeholder="Site" class="black-border" id="">
-                <input type="text" name="uf" placeholder="Estado" class="black-border" id="">
-                <input type="text" name="email" placeholder="E-mail" class="black-border" id="">
-                <button type="submit" class="black-border">Pesquisar</button>
-            </form>
+    
+        <div class="menu-2" style="">
+            <ul>
+                <li>
+                    <a href="#" style="color: white;">Novo</a>
+                    <a href="#" style="color: white;">Consulta</a>
+                </li>
+            </ul>
         </div>
-    </div>
+    
+        <div class="page-information blue-ocean">
+            <div style="width: 30%; margin-left: auto; margin-right: auto; color: black;">
+                <form method="post" action="">
+                    <input type="text" name="name" placeholder="Nome" class="black-border" id="">
+                    <input type="text" name="site" placeholder="Site" class="black-border" id="">
+                    <input type="text" name="uf" placeholder="Estado" class="black-border" id="">
+                    <input type="text" name="email" placeholder="E-mail" class="black-border" id="">
+                    <button type="submit" class="black-border">Pesquisar</button>
+                </form>
+            </div>
+        </div>
+    
 </div>
 @endsection
